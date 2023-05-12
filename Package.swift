235586d073822c404fc1e90369d56263840a6e7f
rintoandrewsaccenture <132916186+rintoandrewsaccenture@users.noreply.github.com>
2023-05-12 16:06:33 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,9 +19,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        
+        
+         .target(
             name: "material-components-ios",
             path: "components",
-            dependencies: []),
+            exclude: [
+                "FBShimmering-Prefix.pch"
+            ],
+            publicHeadersPath: "../FBShimmering"
+        )
     ]
 )
