999d7657e67b7567d1e62996f7faaa48ea48c99b
rintoandrewsaccenture <132916186+rintoandrewsaccenture@users.noreply.github.com>
2023-05-12 16:08:22 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,10 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         
         
-         .target(
-            name: "material-components-ios",
-            path: "components"
-           
-        )
+     .target(
+            name: "Shimmer",
+            dependencies: []),
     ]
 )
